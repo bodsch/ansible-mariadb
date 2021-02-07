a1e07b4ec1c41c3bf936a70174066228de82e5b9
Bo.Sch <bodo@boone-schulz.de>
2021-02-07 20:37:18 +0100

    general update and add github actions (#3)
    * general update and add github actions
    * fix build problems with debian9 and python2

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,53 +1,91 @@
+
+from ansible.parsing.dataloader import DataLoader
+from ansible.template import Templar
 import pytest
 import os
+
 import testinfra.utils.ansible_runner
 
-# import mysql.connector as mysql
-# from mysql.connector import Error
-
+import pprint
+pp = pprint.PrettyPrinter()
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
+def base_directory():
+    cwd = os.getcwd()
+
+    if('group_vars' in os.listdir(cwd)):
+        directory = "../.."
+        molecule_directory = "."
+    else:
+        directory = "."
+        molecule_directory = "molecule/{}".format(os.environ.get('MOLECULE_SCENARIO_NAME'))
+
+    return directory, molecule_directory
+
+
 @pytest.fixture()
 def get_vars(host):
-    defaults_files = "file=../../defaults/main.yml name=role_defaults"
-    vars_files = "file=../../vars/main.yml name=role_vars"
+    """
 
-    ansible_vars = host.ansible(
-        "include_vars",
-        defaults_files)["ansible_facts"]["role_defaults"]
+    """
+    base_dir, molecule_dir = base_directory()
 
-    ansible_vars.update(host.ansible(
-        "include_vars",
-        vars_files)["ansible_facts"]["role_vars"])
+    file_defaults = "file={}/defaults/main.yml name=role_defaults".format(base_dir)
+    file_vars = "file={}/vars/main.yml name=role_vars".format(base_dir)
+    file_molecule = "file={}/group_vars/all/vars.yml name=test_vars".format(molecule_dir)
 
-    print(ansible_vars)
+    defaults_vars = host.ansible("include_vars", file_defaults).get("ansible_facts").get("role_defaults")
+    vars_vars = host.ansible("include_vars", file_vars).get("ansible_facts").get("role_vars")
+    molecule_vars = host.ansible("include_vars", file_molecule).get("ansible_facts").get("test_vars")
 
-    return ansible_vars
+    ansible_vars = defaults_vars
+    ansible_vars.update(vars_vars)
+    ansible_vars.update(molecule_vars)
+
+    templar = Templar(loader=DataLoader(), variables=ansible_vars)
+    result = templar.template(ansible_vars, fail_on_undefined=False)
+
+    return result
 
 
 def test_data_directory(host, get_vars):
-    dir = host.file(get_vars['mariadb_datadir'])
+
+    directory = get_vars.get('mariadb_datadir')
+    user = get_vars.get('mariadb_log_file_group')
+    pp.pprint(directory)
+    pp.pprint(user)
+
+    dir = host.file(directory)
     assert dir.exists
     assert dir.is_directory
-    assert dir.user == get_vars['mariadb_log_file_group']
-    assert dir.group == get_vars['mariadb_log_file_group']
+    assert dir.user == user
+    assert dir.group == user
 
 
 def test_tmp_directory(host, get_vars):
-    dir = host.file(get_vars['mariadb_tmpdir'])
+
+    directory = get_vars.get('mariadb_tmpdir')
+
+    dir = host.file(directory)
     assert dir.exists
     assert dir.is_directory
 
 
 def test_log_directory(host, get_vars):
-    dir = host.file(get_vars['mariadb_log_directory'])
+
+    directory = get_vars.get('mariadb_log_directory')
+    user = get_vars.get('mariadb_log_file_group')
+    pp.pprint(directory)
+    pp.pprint(user)
+
+    dir = host.file(directory)
     assert dir.exists
     assert dir.is_directory
-    assert dir.user == get_vars['mariadb_log_file_group']
-    assert dir.group == get_vars['mariadb_log_file_group']
+    assert dir.user == user
+    assert dir.group == user
 
 
 @pytest.mark.parametrize("dirs", [
@@ -83,46 +121,56 @@ def test_files(host, files):
     assert f.is_file
 
 
-def test_user(host):
-    assert host.group("mysql").exists
-    assert host.user("mysql").exists
-    assert "mysql" in host.user("mysql").groups
-    # assert host.user("mysql").shell == "/bin/false"
-    # assert host.user("mysql").home == "/nonexistent"
+def test_user(host, get_vars):
+
+    distribution = host.system_info.distribution
+
+    if(distribution in ['debian', 'ubuntu']):
+        shell = '/bin/false'
+    elif(distribution in ['centos', 'redhat']):
+        shell = '/sbin/nologin'
+
+    user_name = "mysql"
+    u = host.user(user_name)
+    g = host.group(user_name)
+
+    assert g.exists
+    assert u.exists
+    assert user_name in u.groups
+    assert u.shell == shell
 
 
-# @pytest.mark.parametrize('protocol,port', [
-#     ('unix', '/run/mysqld/mysqld.sock')
-# ])
-# def test_listening_socket(host, protocol, port):
-#     socket = host.socket('%s://%s' % (protocol, port))
-#     assert socket.is_listening
-#
-#
-# def test_mariadb_running_and_enabled(host):
-#     service = host.service("mariadb")
-#     assert service.is_running
-#     assert service.is_enabled
-#
-#
-# def test_mysql_connetc(host):
-#
-#     conn = None
-#     try:
-#       conn = mysql.connect(
-#         host = 'localhost',
-#         user = 'monitoring',
-#         password = 'monitoring',
-#         database = 'monitoring')
-#
-#       assert conn.is_connected()
-#
-#       if conn.is_connected():
-#         print('Connected to MySQL database')
-#
-#     except Error as e:
-#       print(e)
-#
-#     finally:
-#       if conn is not None and conn.is_connected():
-#         conn.close()
+def test_mariadb_running_and_enabled(host, get_vars):
+
+    distribution = host.system_info.distribution
+
+    if(distribution == 'ubuntu'):
+        distribution = 'debian'
+    elif(distribution == 'centos'):
+        distribution = 'redhat'
+
+    service_name = get_vars.get('_mariadb_service').get(distribution)
+
+    service = host.service(service_name)
+    assert service.is_running
+    assert service.is_enabled
+
+
+def test_listening_socket(host, get_vars):
+
+    for i in host.socket.get_listening_sockets():
+        pp.pprint(i)
+
+    distribution = host.system_info.distribution
+
+    if(distribution == 'ubuntu'):
+        distribution = 'debian'
+    elif(distribution == 'centos'):
+        distribution = 'redhat'
+
+    socket_name = get_vars.get('_mariadb_socket').get(distribution)
+    socket_name = 'unix://{}'.format(socket_name)
+    # pp.pprint(socket_name)
+    socket = host.socket(socket_name)
+
+    assert socket.is_listening
